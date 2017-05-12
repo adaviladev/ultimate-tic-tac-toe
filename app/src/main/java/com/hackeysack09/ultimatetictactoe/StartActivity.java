de3a2388e46c137bde5d80ad4861363dffd042e1
Cesar Salazar <cesarsalazar09@gmail.com>
2017-05-12 15:44:39 -0500

    Added functionality to Button, added ImageView that used background.png

--- app/src/main/java/com/hackeysack09/ultimatetictactoe/StartActivity.java
+++ app/src/main/java/com/hackeysack09/ultimatetictactoe/StartActivity.java
@@ -2,6 +2,8 @@ package com.hackeysack09.ultimatetictactoe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class StartActivity extends Activity {
@@ -11,8 +13,16 @@ public class StartActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        final Button click = (Button) findViewById(R.id.button);
 
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = (click.getText().equals(getResources().getString(R.string.button_string)))? "This Works!": getResources().getString(R.string.button_string);
 
+                click.setText(result);
+            }
+        });
     }
 
 
