1be653262bb73e5c793b301efa9afa3ae2dbb20a
adrian <adavila.dev@gmail.com>
2017-05-13 00:57:08 -0500

    Started working on Game interface.

---
+++ app/src/main/java/com/hackeysack09/ultimatetictactoe/GameActivity.java
@@ -0,0 +1,15 @@
+package com.hackeysack09.ultimatetictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
