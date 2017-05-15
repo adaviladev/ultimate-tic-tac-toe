9b6210a67bf931ab6711a38306fd283f06c9d062
Cesar Salazar <cesarsalazar09@gmail.com>
2017-05-15 12:35:14 -0500

    Got rid of first button

--- app/src/main/java/com/hackeysack09/ultimatetictactoe/StartActivity.java
+++ app/src/main/java/com/hackeysack09/ultimatetictactoe/StartActivity.java
@@ -4,30 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class StartActivity extends Activity {
-  private Button click;
-  private Button twoPlayerLocalButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_start);
-
-    click = (Button) findViewById(R.id.button);
-
-    click.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        String result = (click.getText().equals(getResources().getString(R.string.button_string)))
-                        ? "This Works!" : getResources().getString(R.string.button_string);
-
-        click.setText(result);
-
-        System.out.println("Switch");
-      }
-    });
 
   }
 
